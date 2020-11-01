a6286bf79f044f856ee70de121541dd0a0ee1652
avinash-vk <avinash2000vk@gmail.com>
2020-11-01 06:00:59 +0530

    added story ending

---
+++ data.js
@@ -0,0 +1,34 @@
+const prompts= [
+  {
+    id:1,
+    content:"A scary doll comes to life"
+  },
+  {
+    id:2,
+    content:"A scene from a nightmare comes true the next day."
+  },
+  {
+    id:3,
+    content:"Days go by, and your parents don’t come home."
+  },
+  {
+    id:4,
+    content:"You feel yourself slowly becoming a monster."
+  },
+  {
+    id:5,
+    content:"Your friends start to disappear, and no one else notices."
+  },
+  {
+    id:6,
+    content:"You’re lost in the woods, and you don’t know how you got there."
+  }
+]
+
+const creepyGifs = [
+  "https://i.gifer.com/kgY.gif",
+  "https://media.giphy.com/media/aCRTblAWKJroc/giphy.gif",
+  "https://media.giphy.com/media/5fOiRnJOUnTMY/giphy.gif",
+]
+
+module.exports = {prompts,creepyGifs}
--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 require("dotenv").config();
+const {prompts,creepyGifs} = require("./data")
 var async = require('async');
 var pg = require('pg');
 
@@ -6,6 +7,7 @@ const Discord = require("discord.js");
 const bot = new Discord.Client();
 
 bot.login(process.env.DISCORD_TOKEN);
+
 
 
 var config = {
@@ -23,8 +25,38 @@ bot.on("ready", () => {
   console.info(`Logged in as ${bot.user.tag}!`)
 })
 
+const PREFIX ="!boo "
+
+let story = []
+let flag = false;
+const constructStory = () => {
+  let str = story.join(" ")
+  return str;
+}
+
 bot.on("message", msg => {
-  if(msg.content.split(" ")[1] == "start" && msg.mentions.has(bot.user)){
-    msg.channel.send("STARTING...")
+
+  if(msg.content == `${PREFIX}start`){
+
+    story = []
+    let index = Math.floor(Math.random() * 6);
+    msg.channel.send(prompts[index].content);
+    flag=1;
   }
+
+  else if (msg.content == `${PREFIX}end`){
+    let index = Math.floor(Math.random() * 3);
+    msg.channel.send(`The end. Or is it?\n${creepyGifs[index]}`);
+    flag=0;
+    setTimeout(()=>{
+      msg.channel.send(constructStory())
+    },2000)
+  }
+
+  else if(flag){
+    story.push(msg.content);
+    msg.react('💀')
+  }
+
+
 })
